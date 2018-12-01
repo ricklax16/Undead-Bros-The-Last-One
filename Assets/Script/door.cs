dfbfb4875ebb95b7c6fa0da7676854e8577fcb84
ricklax16 <rholden650@g.rwu.edu>
2018-12-01 15:19:54 -0500

    load level 2

--- Assets/Script/door.cs
+++ Assets/Script/door.cs
@@ -1,22 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class door : MonoBehaviour {
     public static int points;
     public  Text tPoints;
    public Sprite open;
- 
+    public string level = "Level 2";
 
     // Use this for initialization
     void Start () {
         points = 0;
        
     }
-	
-	// Update is called once per frame
-	void Update () {
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+        // Update is called once per frame
+        void Update () {
         
        tPoints.text = "Points: " + points.ToString();
 
